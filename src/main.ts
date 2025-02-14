42ee171936cc9586ef0f60e4c8af5108a3934320
krupanjac <arsen.djurdjev@gmail.com>
2025-02-14 20:17:09 +0100

    Refactor header component structure and improve loading screen styling

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { RouterModule, provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes)]
+  providers: [
+    provideRouter(appRoutes),
+    importProvidersFrom(RouterModule) // This line provides ActivatedRoute and other router services globally
+  ]
 }).catch(err => console.error(err));
